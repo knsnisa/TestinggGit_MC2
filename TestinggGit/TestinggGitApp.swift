1cc0e56fe9ab81213a3a9686a566d38469edf642
Khairunnsia Nurrahmatia <72216135+knsnisa@users.noreply.github.com>
2023-05-26 15:41:20 +0700

    Initial Commit

---
+++ TestinggGit/TestinggGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestinggGitApp.swift
+//  TestinggGit
+//
+//  Created by Khairunnisa Nurrahmatia Salsabila on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TestinggGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
