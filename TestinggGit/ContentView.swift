ebd771909ca9cd5a0df90fe99e0775d822b6ad08
Khairunnsia Nurrahmatia <72216135+knsnisa@users.noreply.github.com>
2023-05-26 15:58:17 +0700

    Commit Orang pertama
    done content view

--- TestinggGit/ContentView.swift
+++ TestinggGit/ContentView.swift
@@ -5,22 +5,66 @@
 //  Created by Khairunnisa Nurrahmatia Salsabila on 26/05/23.
 //
 
+
+
 import SwiftUI
 
+// Orang ke 1
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+            }
+            .listStyle(.plain)
+            .navigationTitle("Awesome List")
         }
-        .padding()
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListRowView: View {
+    var body: some View {
+        NavigationLink(destination: Text("Tolong dibikin bro!")) {
+            HStack(alignment: .top) {
+                Rectangle()
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.blue)
+                
+                VStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: 80, height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                }
+                .foregroundColor(.orange)
+            }
+            .padding(.vertical, 10)
+        }
     }
 }
